99c8630cfd512e3e94ed260301cac77f9b197b11
Deepak tiwari <atrivedi8988@gmail.com>
2022-09-11 03:36:26 +0530

    done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>My first deploy react app</h1>
       <h2>My name is aman trivedi</h2>
+      <h1>Now i am starting my portfolio</h1>
     </div>
   );
 }
