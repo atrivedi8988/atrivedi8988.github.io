b93709f13cfae5a9a2b28c3c59bae8e9e11ec392
Deepak tiwari <atrivedi8988@gmail.com>
2022-09-11 02:53:13 +0530

    done deployment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My first deploy react app</h1>
+      <h2>My name is aman trivedi</h2>
+    </div>
+  );
+}
+
+export default App;
